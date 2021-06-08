235c6de975e4f27c35033a49002cd12a10fadd7d
sylar59 <kevin.bajard@hotmail.com>
2021-06-08 07:34:20 +0200

    Création des pages de connexion et d'inscription

---
+++ src/Controller/RegisterController.php
@@ -0,0 +1,55 @@
+<?php
+
+namespace App\Controller;
+
+use App\Entity\User;
+use App\Form\RegisterType;
+use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
+use Symfony\Component\Routing\Annotation\Route;
+
+
+class RegisterController extends AbstractController
+{
+    private $entityManager;
+
+    public function __construct(EntityManagerInterface $entityManager){
+        $this->entityManager = $entityManager;
+    }
+    /**
+     * @Route("/inscription", name="register")
+     */
+    public function index(Request $request, UserPasswordHasherInterface $encoder)
+    {
+        $notification = null;
+        $user = new User();
+        $form = $this->createForm(RegisterType::class,$user);
+
+        $form->handleRequest($request);
+
+        if($form->isSubmitted() && $form->isValid()){
+            $user = $form->getData();
+
+            $search_mail = $this->entityManager->getRepository(User::class)->findOneByEmail($user->getEmail());
+
+            if(!$search_mail){
+                $password = $encoder->hashPassword($user,$user->getPassword());
+                $user->setPassword($password);
+
+                $this->entityManager->persist($user);
+                $this->entityManager->flush();
+                //TODO : quand la classe mail sera en place, envoyer un mail ici
+                $notification = "Votre inscription s'est correctement déroulée. Vous pouvez dès à présent vous connectez à votre compte.";
+            }else{
+                $notification = "L'email que vous avez renseigné existe déjà.";
+            }
+        }
+
+        return $this->render('register/index.html.twig',[
+            'form'=>$form->createView(),
+            'notification' => $notification
+        ]);
+    }
+}
