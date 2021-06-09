9b4ee6484282b276d372b83397e08bdbfe4fac70
sylar59 <kevin.bajard@hotmail.com>
2021-06-09 04:33:13 +0200

    Mise en place du backoffice

---
+++ src/Controller/Admin/ProductCrudController.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace App\Controller\Admin;
+
+use App\Entity\Product;
+use EasyCorp\Bundle\EasyAdminBundle\Controller\AbstractCrudController;
+use EasyCorp\Bundle\EasyAdminBundle\Field\ArrayField;
+use EasyCorp\Bundle\EasyAdminBundle\Field\AssociationField;
+use EasyCorp\Bundle\EasyAdminBundle\Field\ImageField;
+use EasyCorp\Bundle\EasyAdminBundle\Field\MoneyField;
+use EasyCorp\Bundle\EasyAdminBundle\Field\SlugField;
+use EasyCorp\Bundle\EasyAdminBundle\Field\TextareaField;
+use EasyCorp\Bundle\EasyAdminBundle\Field\TextField;
+
+class ProductCrudController extends AbstractCrudController
+{
+    public static function getEntityFqcn(): string
+    {
+        return Product::class;
+    }
+
+
+    public function configureFields(string $pageName): iterable
+    {
+        return [
+            TextField::new('name','Nom'),
+            SlugField::new('slug')->setTargetFieldName('name'),
+            ImageField::new('illustration','Image')
+                ->setBasePath('uploads/')
+                ->setUploadDir('public/uploads')
+                ->setUploadedFileNamePattern('[randomhash].[extension]')
+                ->setRequired(false),
+            TextareaField::new('description','Description'),
+            MoneyField::new('price','Prix')->setCurrency('EUR'),
+            AssociationField::new('category','Catégorie'),
+            ArrayField::new('subcategory','Sous-catégorie(s)')
+        ];
+    }
+
+}
