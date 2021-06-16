e6ebe3975ea7a175286a935641d4e7dcfefa2679
sylar59 <kevin.bajard@hotmail.com>
2021-06-16 20:55:12 +0200

    Configuration pour la migration -> prod

--- src/Controller/Admin/UserCrudController.php
+++ src/Controller/Admin/UserCrudController.php
@@ -5,6 +5,7 @@ namespace App\Controller\Admin;
 use App\Entity\User;
 use EasyCorp\Bundle\EasyAdminBundle\Controller\AbstractCrudController;
 use EasyCorp\Bundle\EasyAdminBundle\Field\ArrayField;
+use EasyCorp\Bundle\EasyAdminBundle\Field\TextField;
 
 class UserCrudController extends AbstractCrudController
 {
@@ -14,14 +15,14 @@ class UserCrudController extends AbstractCrudController
     }
 
 
-/*    public function configureFields(string $pageName): iterable
+    public function configureFields(string $pageName): iterable
     {
         return [
-            IdField::new('id'),
-            TextField::new('title'),
-            TextEditorField::new('description'),
+            TextField::new('email'),
+            TextField::new('firstname'),
+            TextField::new('lastname'),
             ArrayField::new('roles'),
         ];
-    }*/
+    }
 
 }
